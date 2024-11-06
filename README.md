4a864380563a22b32a0ba282114e44608c5743f8
JvictorMarques <jvictorsilvamarques358@gmail.com>
2024-11-06 19:45:27 -0300

    feat: Adicionando a versão final do projeto de computação gráfica
    Foi adicionado todos os metodos novos do GLUT e do OpenGL, com menu e desenhos

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# computacao-grafica
+# Computação Gráfica
+### O ARQUIVO PRINCIPAL É O TESTE
+Este projeto demonstra a criação de gráficos 2D utilizando a biblioteca **OpenGL** com a **FreeGLUT**, permitindo interação através do teclado e do mouse.
+
+## Dependências
+
+Para compilar e executar este projeto, você precisa das seguintes dependências:
+
+- **FreeGLUT**: Biblioteca para criação de janelas e gerenciamento de eventos com OpenGL.
+
+# Instalação do FreeGLUT:
+
+#### No Ubuntu/Debian:
+```bash
+    sudo apt-get install freeglut3 freeglut3-dev
+```
+
+## No Windows:
+Baixe o FreeGLUT do site oficial
+Adicione os arquivos .dll e as bibliotecas ao seu diretório de trabalho.
+Compilação e Execução
+
+## Compilação:
+Para compilar o código, use o seguinte comando:
+
+```bash
+g++ nome_do_arquivo.cpp -o nome_do_projeto -lglut -lGL -lGLU
+```
+
+- **nome_do_arquivo.cpp** é o arquivo de código fonte.
+- **nome_do_projeto** é o nome do executável gerado.
+## Execução:
+Após a compilação, execute o projeto com o comando:
+
+```bash
+./nome_do_projeto
+```
+## Funcionalidades
+1. **Escolha de Cor** (A cor padrão é Branca):
+Pressione r ou R para selecionar Vermelho.
+Pressione g ou G para selecionar Verde.
+Pressione b ou B para selecionar Azul.
+2. **Menu de Interação** (Botão Direito do Mouse):
+Ao clicar com o botão direito do mouse, um menu será exibido com as seguintes opções:
+
+**Pontos**: Clique em qualquer lugar da tela para desenhar pontos na posição do clique.
+**Linha**: Clique com o botão esquerdo para selecionar dois pontos que formarão uma linha entre eles.
+**Retângulo**: Clique com o botão esquerdo para selecionar dois pontos que formarão um retângulo entre eles.
+
+## Exemplos de Uso
+1. Compile e execute o programa.
+2. Clique com o botão direito para abrir o menu de interações.
+3. Escolha a opção Pontos, Linha ou Retângulo.
+4. Selecione as posições na tela para desenhar as formas desejadas.
+5. Use o teclado para alternar entre as cores disponíveis.
--- teste.cpp
+++ teste.cpp
@@ -1,73 +1,160 @@
 #include <cstdlib>
 #include <iostream>
-#include <vector>
 #include <GL/glut.h>
 #include <GL/glu.h>
 
-GLint WINDOW_WIDTH = 500, WINDOW_HEIGHT = 500; // Tamanho da janela
+GLint WINDOW_WIDTH = 500, WINDOW_HEIGHT = 500; 
+int operacao;
+GLint xi, yi, xf, yf;
 
 void init();
-void desenhar(); // Função para desenhar todos os pontos armazenados
+void desenhar();
 void alterar_cor(GLubyte key, GLint x, GLint y);
-void pontos_pressionados(GLint button, GLint action, GLint x, GLint y);// Corrigido para ter parâmetros distintos
+void pontos(GLint x, GLint y); 
+void linhas(GLint button, GLint state, GLint x, GLint y);
+void retangulo(GLint button, GLint state, GLint x, GLint y);
+void criar_menu();
+void menu(int item_number);
 
 int main(int argc, char* argv[])
 {
-    glutInit(&argc, argv); // Inicializa o glut
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de exibição
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     
-    GLint screen_width = glutGet(GLUT_SCREEN_WIDTH);  // Largura do monitor
-    GLint screen_height = glutGet(GLUT_SCREEN_HEIGHT); // Altura do monitor
-    
-    glutInitWindowPosition((screen_width - WINDOW_WIDTH) / 2, (screen_height - WINDOW_HEIGHT) / 2); 
-    // Centraliza a janela
+    GLint screen_width = glutGet(GLUT_SCREEN_WIDTH);
+    GLint screen_height = glutGet(GLUT_SCREEN_HEIGHT);
 
-    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); // Inicializa o tamanho da janela
-    glutCreateWindow("Projeto"); // Cria a janela com o título
+    glutInitWindowPosition((screen_width - WINDOW_WIDTH) / 2, (screen_height - WINDOW_HEIGHT) / 2);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Projeto");
     
     init();
-    glutDisplayFunc(desenhar); // Chama a função desenhar
-    glutMainLoop(); // Inicia o loop principal
+    glutDisplayFunc(desenhar);
+    glutKeyboardFunc(alterar_cor);
+    criar_menu();
+    glutMainLoop();
     
     return EXIT_SUCCESS;
 }
 
 void init()
 {
-    glClearColor(0, 0, 0, 1); // Cor de fundo
-    glMatrixMode(GL_PROJECTION); // Tipo de projeção
-    gluOrtho2D(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT); // Coordenadas da janela
+    glClearColor(0, 0, 0, 1);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT);
+    glFlush();
 }
 
-void desenhar() // Desenha os pontos armazenados
+void desenhar()
 {
-    glClear(GL_COLOR_BUFFER_BIT); // Limpa a janela para desenhar
-    glutKeyboardFunc(alterar_cor); // Registra a função para eventos de teclado
-    glutMouseFunc(pontos_pressionados); // Registra a função para eventos de mouse
-    glFlush(); // Atualiza a janela
+    glClear(GL_COLOR_BUFFER_BIT);
+    switch (operacao)
+    {
+        case 1:
+            glutMotionFunc(pontos);
+            break;
+        case 2:
+            glutMouseFunc(linhas);
+            break;
+        case 3:
+            glutMouseFunc(retangulo);
+            break;
+        default:
+            std::cout << "Operação inválida" << std::endl;
+            break;
+    }
+    glFlush();
 }
 
 void alterar_cor(GLubyte key, GLint x, GLint y)
 {
-    if (key == 'r' || key == 'R') // Cor vermelha
+    if (key == 'r' || key == 'R')
     {
         glColor3f(1, 0, 0);
     }
-    else if (key == 'g' || key == 'G') // Cor verde
+    else if (key == 'g' || key == 'G')
     {
         glColor3f(0, 1, 0);
     }
-    else if (key == 'b' || key == 'B') // Cor azul
+    else if (key == 'b' || key == 'B')
     {
         glColor3f(0, 0, 1);
     }
-    glutPostRedisplay(); // Redesenha a tela
+    glFlush();
 }
 
-void pontos_pressionados(GLint button, GLint action, GLint x, GLint y)
+void criar_menu()
+{
+    GLint menu_id = glutCreateMenu(menu);  
+    glutAddMenuEntry("Ponto", 1);
+    glutAddMenuEntry("Linha", 2);
+    glutAddMenuEntry("Retangulo", 3);  
+    
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void menu(int item_number)
+{
+    operacao = item_number;
+    desenhar();
+}
+
+void pontos(GLint x, GLint y)
 {
     glBegin(GL_POINTS);
-        glVertex2f(x, y); // Inverte a coordenada y para a posição correta
+        glVertex2f(x, WINDOW_HEIGHT - y); 
     glEnd();
-    glutPostRedisplay();
+    glFlush();
+}
+
+void linhas(GLint button, GLint state, GLint x, GLint y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        if (xi == 0 && yi == 0) {
+            xi = x;
+            yi = WINDOW_HEIGHT - y;  
+        }
+        else {
+            xf = x;
+            yf = WINDOW_HEIGHT - y;
+
+            glBegin(GL_LINES);
+                glVertex2f(xi, yi);
+                glVertex2f(xf, yf);
+            glEnd();
+            glFlush();
+            xi = 0;
+            yi = 0;
+            xf = 0;
+            yf = 0;
+        }
+    }
+}
+
+void retangulo(GLint button, GLint state, GLint x, GLint y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        if (xi == 0 && yi == 0) {
+            xi = x;
+            yi = WINDOW_HEIGHT - y;  
+        }
+        else {
+            xf = x;
+            yf = WINDOW_HEIGHT - y;  
+            
+            glBegin(GL_LINE_LOOP);
+                glVertex2f(xi, yi);
+                glVertex2f(xi, yf);
+                glVertex2f(xf, yf);
+                glVertex2f(xf, yi);
+            glEnd();
+            glFlush();
+            xi = 0;
+            yi = 0;
+            xf = 0;
+            yf = 0;
+        }
+    }
 }
